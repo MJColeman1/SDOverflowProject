a60b73299765b87453cfc15740ccf20d355ba554
Rebecca Allen <rebeccazarsky@gmail.com>
2018-05-31 17:25:53 -0600

    add data test stubs to rest

---
+++ OverflowRest/src/test/java/com/skilldistillery/overflow/datatTests/CommentDataTests.java
@@ -0,0 +1,41 @@
+package com.skilldistillery.overflow.datatTests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.skilldistillery.overflow.entities.Comment;
+
+class CommentDataTests {
+
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Comment comment;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		emf = Persistence.createEntityManagerFactory("Overflow");
+		em = emf.createEntityManager();
+		comment = em.find(Comment.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		emf.close();
+		comment = null;
+	}
+	
+	@Test
+	@DisplayName("Test Address is correctly mapped")
+	void test_address_mappings() {
+		fail("Not yet implemented");
+	}
+}
---
+++ OverflowRest/src/test/java/com/skilldistillery/overflow/datatTests/ProfileDataTests.java
@@ -0,0 +1,42 @@
+package com.skilldistillery.overflow.datatTests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import com.skilldistillery.overflow.entities.Profile;
+
+class ProfileDataTests {
+
+	private EntityManagerFactory emf;
+	private EntityManager em;
+	private Profile profile;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		emf = Persistence.createEntityManagerFactory("Overflow");
+		em = emf.createEntityManager();
+		profile = em.find(Profile.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		emf.close();
+		profile = null;
+	}
+	
+	@Test
+	@DisplayName("Test Address is correctly mapped")
+	void test_address_mappings() {
+		fail("Not yet implemented");
+	}
+
+}
