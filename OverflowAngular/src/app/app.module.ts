8f2e19c9cc3f16f2976e570c5c2f15192dd62bae
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-05-31 14:58:16 -0600

    add login and user components

--- OverflowAngular/src/app/app.module.ts
+++ OverflowAngular/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
-    NavigationComponent
+    NavigationComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
