7aa0f068c8fa1429e2c9c17eeaba01fb0e85e665
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-06-01 10:50:58 -0600

    add category, employer, technology classes

---
+++ OverflowAngular/src/app/employer.ts
@@ -0,0 +1,10 @@
+import { Technology } from './technology';
+import { Address } from './address';
+
+export class Employer {
+  id: number;
+  name: string;
+  hiring: string;
+  address: Address;
+  technologies: Technology[];
+}
