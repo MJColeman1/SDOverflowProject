f9d7de1198db89a1645a65ac90d4ca91a338d267
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-06-01 14:03:59 -0600

    add ability to view categories

--- OverflowAngular/src/app/post.service.ts
+++ OverflowAngular/src/app/post.service.ts
@@ -1,3 +1,4 @@
+import { Category } from './category';
 import { Post } from './post';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
@@ -10,12 +11,22 @@ import { catchError, tap } from 'rxjs/operators';
 export class PostService {
 
   private url = 'http://localhost:8080/api/posts/';
+  private categoryUrl = 'http://localhost:8080/api/categories/';
 
   index() {
     return this.http.get<Post[]>(this.url).pipe(
       catchError((err: any) => {
         console.log(err);
         return throwError('Index Error');
+      })
+    );
+  }
+
+  getCategories() {
+    return this.http.get<Category[]>(this.categoryUrl).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('Category Error');
       })
     );
   }
