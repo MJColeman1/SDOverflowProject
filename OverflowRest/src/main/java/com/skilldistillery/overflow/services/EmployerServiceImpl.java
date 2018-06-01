8e0d780d46b91158242bf66a77e4253232b9d72f
Rebecca Allen <rebeccazarsky@gmail.com>
2018-06-01 13:27:44 -0600

    make employer dto, create employer and address method, change employer controller

--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/EmployerServiceImpl.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/EmployerServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.overflow.entities.Address;
 import com.skilldistillery.overflow.entities.Employer;
+import com.skilldistillery.overflow.entities.EmployerDTO;
 import com.skilldistillery.overflow.entities.User;
 import com.skilldistillery.overflow.respositories.EmployerRepository;
 import com.skilldistillery.overflow.respositories.UserRepository;
@@ -29,16 +31,30 @@ public class EmployerServiceImpl implements EmployerService{
 	}
 
 	@Override
-	public Employer createNewEmployer(int userId, Employer employer, String username) {
+	public Employer createNewEmployer(int userId, EmployerDTO dto, String username) {
 		User user = userRepo.findById(userId).get();
 		if (user.getUsername().equals(username)) {
+			// MAKE A NEW ADDRESS WITH DTO FORM DATA
+			Address address = new Address();
+			address.setStreet(dto.getEmployerStreet());
+			address.setStreet(dto.getEmployerStreet2());
+			address.setCity(dto.getEmployerCity());
+			address.setState(dto.getEmployerState());
+			address.setCountry(dto.getEmployerCountry());
+			address.setZip(dto.getEmployerZip());
+			// MAKE A NEW EMPLOYER WITH DTO FORM DATA, SET ADDRESS AS IT'S ADDRESS
+			Employer employer = new Employer();
+			employer.setName(dto.getEmployerName());
+			employer.setHiring(dto.getEmployerHiring());
+			employer.setAddress(address);
+			// SAVE EMPLOYER, ADDRESS WILL BE CASCADED
 			empRepo.saveAndFlush(employer);
 		}
 		return null;
 	}
 
 	@Override
-	public Employer updateEmployerById(int userId, int empId, Employer employer, String username) {
+	public Employer updateEmployerById(int userId, int empId, EmployerDTO dto, String username) {
 		// TODO Auto-generated method stub
 		return null;
 	}
