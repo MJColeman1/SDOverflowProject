b1ca27bccf819b0de93a235ca5afdfcc23c70984
Mark Coleman <setyourgoalsss@gmail.com>
2018-06-01 14:01:43 -0600

    completed tech service and controller for full CRUD functionality

--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/TechnologyService.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/TechnologyService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.overflow.services;
 
+import java.util.List;
+
+import com.skilldistillery.overflow.entities.Technology;
+
 public interface TechnologyService {
+	
+	public List<Technology> index();
+	
+	public Technology findById(int techId);
+	
+	public Technology create(Technology tech);
+	
+	public Technology update(Technology tech, int techId);
+	
+	public boolean destroy(int techId);
 
 }
--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/TechnologyServiceImpl.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/TechnologyServiceImpl.java
@@ -1,8 +1,55 @@
 package com.skilldistillery.overflow.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.overflow.entities.Technology;
+import com.skilldistillery.overflow.respositories.TechnologyRepository;
 
 @Service
 public class TechnologyServiceImpl implements TechnologyService{
+
+	@Autowired
+	private TechnologyRepository techRepo;
+	
+	@Override
+	public List<Technology> index() {
+		return techRepo.findAll();
+	}
+
+	@Override
+	public Technology findById(int techId) {
+		return techRepo.findById(techId).get();
+	}
+
+	@Override
+	public Technology create(Technology tech) {
+		return techRepo.saveAndFlush(tech);
+	}
+
+	@Override
+	public Technology update(Technology tech, int techId) {
+		Technology managed = techRepo.findById(techId).get();
+		managed.setName(tech.getName());
+		managed.setDescription(tech.getDescription());
+		managed.setPayscale(tech.getPayscale());
+		
+		return techRepo.saveAndFlush(managed);
+	}
+
+	@Override
+	public boolean destroy(int techId) {
+		Technology tech = techRepo.findById(techId).get();
+		try {
+			techRepo.delete(tech);
+			return true;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 
 }
