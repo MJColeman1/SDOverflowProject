cc804178616527b3a6c81668af69b85e16a4b921
Rebecca Allen <rebeccazarsky@gmail.com>
2018-06-01 09:03:09 -0600

    i really hope this doesn't cause a merge conflict

--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/UserServiceImpl.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/UserServiceImpl.java
@@ -1,17 +1,15 @@
 package com.skilldistillery.overflow.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import com.skilldistillery.overflow.entities.Address;
-import com.skilldistillery.overflow.entities.Employer;
 import com.skilldistillery.overflow.entities.Profile;
-import com.skilldistillery.overflow.entities.ProfileDTO;
-import com.skilldistillery.overflow.entities.Technology;
 import com.skilldistillery.overflow.entities.User;
 import com.skilldistillery.overflow.entities.UserDTO;
 import com.skilldistillery.overflow.respositories.UserRepository;
 
-public class UserServiceImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService{
 
 	@Autowired
 	private UserRepository userRepo;
@@ -20,18 +18,21 @@ public class UserServiceImpl implements UserService {
 	public User createUser(UserDTO dto) {
 		User user = new User();
 		Profile profile = new Profile();
-
+		
 		user.setUsername(dto.getUserUsername());
 		user.setPassword(dto.getProfileEmail());
 		profile.setFirstName(dto.getProfileFirstName());
 		profile.setLastName(dto.getProfileLastName());
 		profile.setEmail(dto.getProfileEmail());
-
-		user.setProfile(profile);
-
-		return userRepo.saveAndFlush(user);
 		
+		user.setProfile(profile);
+		
+		
+		 userRepo.saveAndFlush(user);
+		 return user;
 	}
+
+
 
 	@Override
 	public boolean deleteUser(int userId) {
@@ -42,7 +43,7 @@ public class UserServiceImpl implements UserService {
 			e.printStackTrace();
 		}
 		return false;
-
+	
 	}
 
 }
