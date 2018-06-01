3bd6947c82d9415c23bd6efe9e8e099777e7a24f
miranda <chopsticksally@yahoo.com>
2018-06-01 10:38:14 -0600

    queries moved to comment and post repos for testing

--- OverflowRest/src/main/java/com/skilldistillery/overflow/respositories/CommentRepository.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/respositories/CommentRepository.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.overflow.respositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.overflow.entities.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
+//	@Query("SELECT u.comments from User u JOIN FETCH u.comments where u.id = :id")
+//	public List<Comment>getCommentsforUser(@Param("id")int id);
 }
--- OverflowRest/src/main/java/com/skilldistillery/overflow/respositories/PostRepository.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/respositories/PostRepository.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.overflow.respositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.overflow.entities.Post;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
 
+	//@Query("SELECT u.posts from User u JOIN FETCH u.posts where u.id = :id")
+	//public List<Post>getPostsForUser(@Param("id")int id);
 }
