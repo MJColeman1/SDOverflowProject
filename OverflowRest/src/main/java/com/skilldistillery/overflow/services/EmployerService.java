78b54f3833fc4b82c62f2d42655a27e8b95e3f89
Xian Zheng <xzheng2112@gmail.com>
2018-06-01 10:52:14 -0600

    EmployerController created, stubbed methods in EmployerService

--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/EmployerService.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/EmployerService.java
@@ -1,5 +1,19 @@
 package com.skilldistillery.overflow.services;
 
-public interface EmployerService {
+import java.util.List;
 
+import com.skilldistillery.overflow.entities.Employer;
+
+public interface EmployerService {
+	
+	List<Employer> getAllEmployers();
+	
+	Employer findEmployerById(int empId);
+	
+	Employer createNewEmployer(int userId, int empId, Employer employer, String username);
+	
+	Employer updateEmployerById(int userId, int empId, Employer employer, String username);
+	
+	Boolean deleteEmployerById(int userId, int empId, String username);
+	
 }
--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/EmployerServiceImpl.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/EmployerServiceImpl.java
@@ -1,8 +1,44 @@
 package com.skilldistillery.overflow.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.skilldistillery.overflow.entities.Employer;
+import com.skilldistillery.overflow.respositories.EmployerRepository;
 
 @Service
 public class EmployerServiceImpl implements EmployerService{
+	@Autowired
+	private EmployerRepository empRepo;
+
+	@Override
+	public List<Employer> getAllEmployers() {
+		return empRepo.findAll();
+	}
+
+	@Override
+	public Employer findEmployerById(int empId) {
+		return empRepo.findById(empId).get();
+	}
+
+	@Override
+	public Employer createNewEmployer(int userId, int empId, Employer employer, String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employer updateEmployerById(int userId, int empId, Employer employer, String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Boolean deleteEmployerById(int userId, int empId, String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
