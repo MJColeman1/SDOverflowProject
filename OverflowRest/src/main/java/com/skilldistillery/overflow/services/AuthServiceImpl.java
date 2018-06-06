4609fb2ba1d41cf7ee01c4844d4c13bbaab1297a
Rebecca Allen <rebeccazarsky@gmail.com>
2018-06-06 11:35:00 -0600

    user is not being persisted in angular

--- OverflowRest/src/main/java/com/skilldistillery/overflow/services/AuthServiceImpl.java
+++ OverflowRest/src/main/java/com/skilldistillery/overflow/services/AuthServiceImpl.java
@@ -8,13 +8,9 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.skilldistillery.overflow.entities.User;
 import com.skilldistillery.overflow.entities.UserDTO;
-import com.skilldistillery.overflow.respositories.UserRepository;
 
 @Service
 public class AuthServiceImpl implements AuthService {
-	
-	@Autowired
-	private UserRepository userRepo;
 	
 	@Autowired
 	private UserService userService;
@@ -26,15 +22,12 @@ public class AuthServiceImpl implements AuthService {
 	public User register(String json) {
 		ObjectMapper om = new ObjectMapper();
 		UserDTO dto = null;
-		User user = new User();
+		User user = null;
 		try {
 			dto = om.readValue(json, UserDTO.class);
 			String encodedPW = encoder.encode(dto.getUserPassword());
 			dto.setUserPassword(encodedPW);
-			user.setEnabled(true);
-			user.setRole("standard");
 			user = userService.createUser(dto);
-			userRepo.saveAndFlush(user);
 		} catch (Exception e) {
 			System.out.println(e);
 		}		
